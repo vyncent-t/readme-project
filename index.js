16bb693dfc63dcaaea0baf4c5490a9e829c10957
Vincent T <vyncode@gmail.com>
2021-05-03 21:06:06 -0400

    finished logic for read me maker

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const questions = [
     "What problem does this project aim to solve?",
     "explain and provide the installation instructions",
     "provide examples for uses of this project",
-    "provide relevant license information for this project",
+    "choose relevant license for this project",
     "provide information on and for contributors (if none please state so)",
     "provide relevant testing information",
     "provide FAQ information",
@@ -60,9 +60,10 @@ function init() {
             message: questions[5]
         },
         {
-            type: "input",
+            type: "list",
             name: "license",
-            message: questions[6]
+            message: questions[6],
+            choices: ['apache','bsd','mit','none']
         },
         {
             type: "input",
@@ -110,6 +111,58 @@ function init() {
         
         let userLicense = result.license
         console.log(`license info: ${userLicense}`)
+
+    let alicense = `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+
+    let alicenseInfo = `Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.`
+
+
+
+    let blicense = `[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)`
+
+        let blicenseInfo = `Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.`
+
+    let mlicense = `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
+
+    let mlicenseInfo = `Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.`
+
+    let nlicense = " "
+
+        let licenseInfo;
+        let licenseText;
+
+        if(userLicense == 'apache') {
+            licenseInfo = alicense
+            licenseText = alicenseInfo
+        } else if (userLicense == 'bsd') {
+            licenseInfo = blicense
+            licenseText = blicenseInfo
+        } else if (userLicense == 'mit') {
+            licenseInfo = mlicense
+            licenseText = mlicenseInfo
+        } else {licenseInfo = nlicense}
         
         let userCont = result.team
         console.log(`contributor info: ${userCont}`)
@@ -126,8 +179,14 @@ function init() {
         let userEmail = result.email
         console.log(`user email: ${userEmail}`)
 
+
+
+
+
+
         let readTemp = `
-# ${userTitle}
+${licenseInfo}
+# ${userTitle} 
 
 ## Description
 ${questions[1]}
@@ -159,6 +218,7 @@ ${questions[3]}
 
 ## License
 - ${userLicense}
+${licenseText}
 
 ## Tests
 - ${userExp}
